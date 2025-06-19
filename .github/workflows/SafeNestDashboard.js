35791fc83c76408d08cd13367fa49c28171a241f
Adrian Serina <adriandonovanserina@gmail.com>
2025-06-19 08:14:35 -0400

    Add files via upload

---
+++ .github/workflows/SafeNestDashboard.js
@@ -0,0 +1,3 @@
+
+// SafeNest AI: Expanded Clickable Prototype
+// [REDACTED FOR LENGTH - loaded from canvas in actual integration]
---
+++ .github/workflows/app.js
@@ -0,0 +1,6 @@
+// Entry point for SafeNest AI React App
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SafeNestDashboard from './SafeNestDashboard';
+
+ReactDOM.render(<SafeNestDashboard />, document.getElementById('root'));
